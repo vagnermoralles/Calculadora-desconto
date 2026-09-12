baaec761d1f18ae277ddfcc183bf2d3610f1a37f
vagnermoralles <322928836+vagnermoralles@users.noreply.github.com>
2026-09-12 09:33:52 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+# 🛒 Calculadora de Desconto Progressivo em Python
+Este é um programa em Python para calcular o valor de desconto e o total a ser pago em uma compra.
+
+---
+
+## 📌 Regras de Negócio
+
+O desconto é aplicado automaticamente com base nas seguintes faixas de valor:
+
+| Valor da Compra | Desconto |
+| :--- | :---: |
+| Menor que R$ 200,00 | **5%** |
+| Entre R$ 200,00 e R$ 299,99 | **10%** |
+| A partir de R$ 300,00 | **15%** |
+
+---
+
+## 🛠️ Funcionalidades
+
+- Leitura do nome do cliente e do valor da compra.
+- Identificação automática da porcentagem de desconto.
+- Exibição do valor economizado em reais (`R$`).
+- Exibição do valor final atualizado a ser pago.
---
+++ app.py
@@ -0,0 +1,23 @@
+# Programa para cálculo de descontos progressivos
+
+#Entrada das informações
+nome = input("Digite seu nome: ")
+valor_compra = float(input("Digite o valor da compra: "))
+
+#Processamento (Escolha adeqquada da porcentagem a ser aplicada)
+if valor_compra < 200.00:
+    desconto_percentual = 5
+elif valor_compra < 300.00:
+    desconto_percentual = 10
+else:
+    desconto_percentual = 15
+
+#Cálculo do desconto
+valor_desconto = valor_compra * (desconto_percentual / 100)
+valor_final = valor_compra - valor_desconto
+
+# Saída 
+print(f"Obrigado pela preferência, {nome}!")
+print(f"Como sua compra teve valor total de: {valor_compra:}")
+print(f"\nDesconto aplicado ({desconto_percentual}%): R$ {valor_desconto:.2f}")
+print(f"Valor total a pagar: R$ {valor_final:.2f}")
